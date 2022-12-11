d7ba5047514cce8e3eafb724d68986fef299359f
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-12-12 01:36:23 +0300

    add IUserService

---
+++ crudapi/Service/IUserService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crudapi.Entities;
+using crudapi.Models.Users;
+
+namespace crudapi.Service
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        void Create(CreateRequest model);
+    }
+}
