9833851edc87db7757538ff22a8b22e0f2a23b87
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-12-12 01:27:14 +0300

    add DataContext

---
+++ crudapi/Helpers/DataContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crudapi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudapi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // in memory database used for simplicity, change to a real db for production applications
+            options.UseInMemoryDatabase("TestDb");
+        }
+
+
+        public DbSet<User>? Users { get; set; }
+    }
+
+}
